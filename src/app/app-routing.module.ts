f47379702e1fdbeb8ff1dc3f74cbf898439ff433
Oderlan <oderlanfreire@gmail.com>
2023-10-27 11:50:45 -0300

    component games criado, rota do component criada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'games'},
+  {
+    path: 'games',
+    loadChildren: () => import('./games/games.module').then(m => m.GamesModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
